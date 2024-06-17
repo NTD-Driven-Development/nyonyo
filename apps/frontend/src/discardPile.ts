5d7ed7d0bc81f499621868eabde07172ecf0a436
= <=>
2024-06-17 14:33:21 +0800

    update.

---
+++ apps/frontend/src/discardPile.ts
@@ -0,0 +1,20 @@
+import { Path, Rectangle, Point, Group } from 'paper/dist/paper-core';
+import { Card } from '~/src/card';
+import _ from 'lodash';
+
+export class DiscardPile extends Group {
+    cards: Card[];
+
+    constructor(position: paper.PointLike) {
+        super();
+        
+        this.applyMatrix = false;
+        this.cards = [];
+
+        const border = new Path.Rectangle(new Rectangle(0, 0, 250, 250));
+        // border.fillColor = new Color(0, 0, 0);
+        this.addChild(border);
+
+        this.position = new Point(position);
+    }
+}
