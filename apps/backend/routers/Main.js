1d2ca24bf624454d25deb9461cb8f7b6d0bf1f54
a23405 <a2340500789@gmail.com>
2024-06-17 17:57:41 +0800

    feat:

--- apps/backend/routers/Main.js
+++ apps/backend/routers/Main.js
@@ -3,7 +3,7 @@ import MainController from "../controllers/MainController.js";
 
 const router = express.Router();
 
-router.post('/start_game', MainController.startGame)
+router.post('/api/startGame', MainController.startGame)
 
 
 
