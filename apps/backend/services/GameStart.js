32468fadcbd04c7ac19c4ddc53c210f9781fe3d3
a23405 <a2340500789@gmail.com>
2024-06-17 18:05:20 +0800

    fix:

--- apps/backend/services/GameStart.js
+++ apps/backend/services/GameStart.js
@@ -1,18 +1,12 @@
 import db from "../models/index.js";
 const RoomMember = db.roomMembers;
+import { v4 } from 'uuid';
 
-class GameStartService {
-    async gameStart(data) {
-        // 寫入對映欄位名稱的資料內容
-        return await RoomMember.create({
-            // 記得 value 字串要加上引號
-            room_id: data.roomId,
-            m1: data.member1,
-            m2: data.member2,
-            m3: data.member3,
-            m4: data.member4,
-        });
+class RoomCreatService {
+    async gameStart() {
+        const game_id = v4();
+        return game_id;
     }
 }
 
-export default new GameStartService();
+export default new RoomCreatService();
